524d77bf58e729178c8ee2f06e3f73803b1f1e7d
Andreas Wilcox <UnknownBum@gmail.com>
2018-08-05 11:38:26 +0200

    Tortoise move speed and input controls

---
+++ Assets/TortoiseAnimationSpeed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TortoiseAnimationSpeed : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Animator>().speed = 2.5f;
+    }
+
+    void Update()
+    {
+
+    }
+}
